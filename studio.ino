2ecde7d9802638f2ee302549d31c422ab8de5962
Murdock73 <Murdock73@users.noreply.github.com>
2020-03-15 17:08:44 +0100

    Improved version

--- studio.ino
+++ studio.ino
@@ -2,16 +2,19 @@
 #include <PubSubClient.h>
 
 // Connection parms
-const char* ssid = "**********";
-const char* password = "**********";
-const char* mqtt_server = "192.168.1.***";
-const char* MQTTuser = "**********";
-const char* MQTTpwd = "**********";
+const char* ssid = "Myssid";
+const char* password = "Mypassword";
+const char* mqtt_server = "192.168.1.100";
+const char* MQTTuser = "Myuser";
+const char* MQTTpwd = "Mypassord";
+const char* studio_topic = "HA/studio/shade/state";
+
 
 // PubSubClient Settings
 WiFiClient espClient;
 PubSubClient client(espClient);
 String switch1;
+String saveswitch1;
 String strTopic;
 String strPayload;
 
@@ -23,7 +26,6 @@ int studiogiu = 0; // Pin per tapparella studio giu
 int studiosu = 2; // Pin per tapparella studio su
 unsigned long startstudio = 0;
 unsigned long endstudio = 23000;
-bool firstshot = false;
 
 void setup_wifi() {
 
@@ -51,45 +53,29 @@ void setup_wifi() {
 void callback(char* topic, byte* payload, unsigned int length) {
   payload[length] = '\0';
   strTopic = String((char*)topic);
-  Serial.println(strTopic);
 
   if(strTopic == "HA/studio/shade"){
     switch1 = String((char*)payload);
+    Serial.println(switch1);
       
     if(switch1 == "STOP") {
-      Serial.println("STOP");
-      digitalWrite(studiosu, LOW);
-      digitalWrite(studiogiu, LOW);
-      delay(10);
       digitalWrite(studiosu, HIGH);
       digitalWrite(studiogiu, HIGH); 
       startstudio = 0;
     }
   
-    if(switch1 == "GIU") {
-      Serial.println("GIU");
-      if (startstudio > 0) {         //se il contatore del tempo è > 0 stoppo i relè
-        digitalWrite(studiogiu, HIGH);
-        digitalWrite(studiosu, HIGH);      
-        startstudio = 0;
-        Serial.print("FINE TAPPARELLA");
-      } else {
-        startstudio = millis();
-        digitalWrite(studiogiu, LOW); 
-      }
+    if(switch1 == "GIU" && switch1 != saveswitch1) {
+      Serial.println("SCENDO");
+      startstudio = millis();
+      digitalWrite(studiogiu, LOW); 
+      saveswitch1 = switch1;
     }
     
-    if(switch1 == "SU") {
-      Serial.println("SU");
-      if (startstudio > 0) {         //se il contatore del tempo è > 0 stoppo i relè
-        digitalWrite(studiogiu, HIGH);
-        digitalWrite(studiosu, HIGH);      
-        startstudio = 0;
-        Serial.print("FINE TAPPARELLA");
-      } else {
-        startstudio = millis();
-        digitalWrite(studiosu, LOW); 
-      }
+    if(switch1 == "SU" && switch1 != saveswitch1) {
+      Serial.println("ALZO");
+      startstudio = millis();
+      digitalWrite(studiosu, LOW); 
+      saveswitch1 = switch1;
     }
   }
 }
@@ -141,7 +127,6 @@ void loop()
       digitalWrite(studiosu, HIGH);      
       startstudio = 0;
       Serial.print("FINE TAPPARELLA");
-      client.publish(studio_topic, "STOP"); // cambio lo stato dello switch MQTT a STOP
     }
   }
 }
