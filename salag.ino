95753976ae730bc8128d6a1e0d929786d5f398b7
Murdock73 <Murdock73@users.noreply.github.com>
2023-06-16 11:48:23 +0200

    Update salag.ino
    Updated to last version

--- salag.ino
+++ salag.ino
@@ -1,12 +1,12 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
 #include <PubSubClient.h>
+#include <ArduinoOTA.h>
 
 // Connection parms
-const char* ssid = "*********";
-const char* password = "************";
-const char* mqtt_server = "192.168.1.100";
-const char* MQTTuser = "***********";
-const char* MQTTpwd = "************";
+#include <SSID.h>
+#include <Salag.h>
 
 // PubSubClient Settings
 WiFiClient espClient;
@@ -20,10 +20,10 @@ String strPayload;
 // Misc variables
 unsigned long timestamp; 
 
-int salaggiu = 0; // Pin per tapparella salag giu
-int salagsu = 2; // Pin per tapparella salag su
-unsigned long startsalag = 0;
-unsigned long endsalag = 31000;
+int salaggiu = 0; // Pin per tapparella salaggiu
+int salagsu = 2; // Pin per tapparella salagsu
+unsigned long startsalag= 0;
+unsigned long endsalag= 31000;
 bool firstshot = false;
 
 void setup_wifi() {
@@ -65,26 +65,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
     
     if(switch1 == "STOP") {
       Serial.println("FERMA TUTTO");
-      digitalWrite(salagsu, LOW);
-      digitalWrite(salaggiu, LOW);
       digitalWrite(salagsu, HIGH);
+      delay(100);
       digitalWrite(salaggiu, HIGH);
-      startsalag = 0;
+      startsalag= 0;
     }
 
     if(switch1 == "GIU" && switch1 != saveswitch1) {
       Serial.println("fai scendere");
-      startsalag = millis();
-      digitalWrite(salaggiu, LOW); 
+      startsalag= millis();
       digitalWrite(salagsu, HIGH); 
+      delay(100);      
+      digitalWrite(salaggiu, LOW); 
       saveswitch1 = switch1;
     }
     
     if(switch1 == "SU" && switch1 != saveswitch1) {
       Serial.println("fai salire");
-      startsalag = millis();
-      digitalWrite(salagsu, LOW);
+      startsalag= millis();
       digitalWrite(salaggiu, HIGH);
+      delay(100);         
+      digitalWrite(salagsu, LOW);
       saveswitch1 = switch1;
     }
   }
@@ -118,24 +119,58 @@ void setup()
   client.setCallback(callback);
   pinMode(salaggiu, OUTPUT);
   digitalWrite(salaggiu, HIGH);
+  delay(100);
   pinMode(salagsu, OUTPUT);
   digitalWrite(salagsu, HIGH);
 
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("esp8266-SALAG");
+
+  // No authentication by default
+  ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
 }
- 
+
 void loop()
 {
+  ArduinoOTA.handle();
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
   
-  if (startsalag > 0) {
-    if ((millis() - startsalag) > endsalag 
+  if (startsalag> 0) {
+    if ((millis() - startsalag) > endsalag
      || (millis() - startsalag) < 0) {
       digitalWrite(salaggiu, HIGH);
+      delay(100);
       digitalWrite(salagsu, HIGH);      
-      startsalag = 0;
+      startsalag= 0;
       Serial.print("FINE TAPPARELLA");
     }
   }
